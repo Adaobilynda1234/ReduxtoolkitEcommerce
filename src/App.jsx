144a17eeb90285632bb176c8558d8972b80afb1b
Adaobilynda1234 <adaobilynda1234@gmail.com>
2025-01-11 23:10:23 +0100

    added images

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,37 @@
-import Navbar from "./Components/Navbar/Navbar";
-export default function App() {
+import React from "react";
+import "./App.css";
+import Main from "./Components/Main/Main";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import FilteredProducts from "./Components/FiltredProducts/FilteredProducts";
+import SingleProduct from "./Components/FiltredProducts/SingleProduct";
+import Login from "./Components/Login/Login";
+import { useSelector } from "react-redux";
+
+function App() {
+  const user = useSelector((state) => state.user.user);
+  const { authUser } = user;
+
   return (
-    <div>
-      <Navbar />
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={authUser ? <Main></Main> : <Login></Login>}
+          ></Route>
+
+          <Route
+            path="/filteredProducts/:type"
+            element={<FilteredProducts></FilteredProducts>}
+          ></Route>
+          <Route
+            path="/filteredProducts/:type/:id"
+            element={<SingleProduct></SingleProduct>}
+          ></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
+
+export default App;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assets/images/storelogo.png";
-// import Cart from "../Cart/Cart";
-// import { useSelector, useDispatch } from "react-redux";
-// import { logout } from "../../features/slices/authSlice";
-// import { Avatar } from "@material-tailwind/react";
-// import { Tooltip } from "@material-tailwind/react";
+import Cart from "../Cart/Cart";
+import { useSelector, useDispatch } from "react-redux";
+import { logout } from "../../features/slices/authSlice";
+import { Avatar } from "@material-tailwind/react";
+import { Tooltip } from "@material-tailwind/react";
 
 const Navbar = () => {
-  //   const totalAmount = useSelector((state) => state.cart.totalAmount);
-  //   const user = useSelector((state) => state.user.user);
-  //   const { name, image } = user;
-  //   const [open, setOpen] = useState(false);
-  //   const handleOpen = () => {
-  //     setOpen(true);
-  //   };
-  //   const dispatch = useDispatch();
+  const totalAmount = useSelector((state) => state.cart.totalAmount);
+  const user = useSelector((state) => state.user.user);
+  const { name, image } = user;
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const dispatch = useDispatch();
 
   return (
     <>
@@ -47,45 +47,53 @@ const Navbar = () => {
               Whish List
             </p>
           </div>
-          <div className="flex flex-row items-center cursor-pointer">
-            {/* {totalAmount > 0 ? (
+          <div
+            className="flex flex-row items-center cursor-pointer"
+            onClick={handleOpen}
+          >
+            {totalAmount > 0 ? (
               <span className="rounded-full bg-gray-300 px-2 font-inter text-sm mr-1">
                 {totalAmount}
               </span>
-            ) : ( */}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="2"
-              stroke="#000"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
-              />
-            </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="#000"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+                />
+              </svg>
+            )}
 
             <p className=" font-inter text-base font-medium tracking-normal leading-none text-center ">
               Shopping bag
             </p>
-            <div></div>
+            <div>
+              {open && <Cart openModal={open} setOpen={setOpen}></Cart>}
+            </div>
           </div>
           <div className="flex flex-row items-center cursor-pointer pl-4">
-            {/* {image && (
+            {image && (
               <Avatar
                 src={image}
                 alt="avatar"
                 size="sm"
                 className="mr-2"
               ></Avatar>
-            )} */}
-            <div>
-              <p className="font-inter text-sm font-medium tracking-normal leading-none">
-                Hi
-              </p>
+            )}
+            <div onClick={() => dispatch(logout())}>
+              <Tooltip content="Sign Out" placement="bottom">
+                <p className="font-inter text-sm font-medium tracking-normal leading-none">
+                  Hi {name.charAt("0").toUpperCase() + name.slice(1)}
+                </p>
+              </Tooltip>
             </div>
           </div>
         </div>
